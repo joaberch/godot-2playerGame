3088d3771ce628883afeef52e7f0874f133d695d
cycoborchp <joachimsimeongabriel.berchel@eduvaud.ch>
2023-11-16 12:39:22 +0100

    Adding the map

--- Godot-2D-mariolike/lion/lion.gd
+++ Godot-2D-mariolike/lion/lion.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @onready var animationsprite = $SpriteSheet/AnimationPlayer
 @onready var player1 = $Player1
 @onready var player2 = $Player2
+var checkslow = 1
 var stringanimation = ""
 
 func _physics_process(_delta):
@@ -42,22 +43,22 @@ func movePlayer():
 	#If the player 2 is the lion
 	if Global.player1isowl:
 		if Input.is_action_pressed("right-D"):
-			velocity.x = Global.lionspeed
+			velocity.x = (Global.lionspeed / checkslow)
 		elif Input.is_action_pressed("left-A"):
-			velocity.x = -Global.lionspeed
+			velocity.x = -(Global.lionspeed / checkslow)
 		if Input.is_action_pressed("down-S"):
-			velocity.y = Global.lionspeed
+			velocity.y = (Global.lionspeed / checkslow)
 		elif Input.is_action_pressed("up-W"):
-			velocity.y = -Global.lionspeed
+			velocity.y = -(Global.lionspeed / checkslow)
 	else: #If the player 1 is the lion
 		if Input.is_action_pressed("right"):
-			velocity.x = Global.lionspeed
+				velocity.x = (Global.lionspeed / checkslow)
 		elif Input.is_action_pressed("left"):
-			velocity.x = -Global.lionspeed
+			velocity.x = -(Global.lionspeed / checkslow)
 		if Input.is_action_pressed("down"):
-			velocity.y = Global.lionspeed
+			velocity.y = (Global.lionspeed / checkslow)
 		elif Input.is_action_pressed("up"):
-			velocity.y = -Global.lionspeed
+			velocity.y = -(Global.lionspeed / checkslow)
 
 func animatePlayer():
 	stringanimation = "RESET"
