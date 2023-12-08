e009615cb8e31aac3d835e53b4577a06cc9ff452
cycoborchp <joachimsimeongabriel.berchel@eduvaud.ch>
2023-12-08 21:28:38 +0100

    Edit the map and the camera speed

--- Godot-2D-mariolike/map3/main_3.gd
+++ Godot-2D-mariolike/map3/main_3.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 @onready var camera = $Camera2D
 @onready var timerCameraspeed2 = $TimerCameraSpeed2
-var cameraSpeed = 0
+@onready var rightBorder = $rightBorder
+var cameraSpeed = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,15 +12,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	camera.offset.x += cameraSpeed
-
+	camera.offset.x += cameraSpeed/10
+	rightBorder.position.x += cameraSpeed/10
 
 func _on_timer_before_camera_move_timeout():
-	cameraSpeed = 1
 	timerCameraspeed2.start()
 
 
 func _on_timer_camera_speed_2_timeout():
-	cameraSpeed += 0.01
-	
+	cameraSpeed += 0.001
 	
