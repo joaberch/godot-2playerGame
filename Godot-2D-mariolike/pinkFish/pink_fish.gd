1c7be6322b5ff36f5a543c9bdee30ca949031193
joaberch <122774888+joaberch@users.noreply.github.com>
2023-12-09 10:36:20 +0100

    Make the fish move at the beginning

--- Godot-2D-mariolike/pinkFish/pink_fish.gd
+++ Godot-2D-mariolike/pinkFish/pink_fish.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
 func _physics_process(_delta):
+	#Check if the player has pressed an input
 	CheckInput()
 	#Make the fish look in the direction it is going (TODO : polish)
-	look_at(velocity*-velocity*velocity)
+	look_at (velocity*-velocity*velocity)
 	#Moving/sliding
 	move_and_slide()
+
+func _ready():
+	velocity.x = Global.pinkFishVelocity
 
 func CheckInput():
 	#Moving to the left
