bef61a25f4d2bbc4c10e6d0d7279674f1bc9100d
cycoborchp <joachimsimeongabriel.berchel@eduvaud.ch>
2024-01-19 19:22:51 +0100

    creating a go game

---
+++ GoBan/9x9Playground/playground_9x_9.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var white = load("res://art/whiteStone.png")
+var black = load("res://art/blackStone.png")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Changes only the arrow shape of the cursor.
+	# This is similar to changing it in the project settings.
+	Input.set_custom_mouse_cursor(black)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
